4303097fef9aaf2f78fe06d471ccee6b020d5055
Ilya Zverev <ilya@zverev.info>
2025-05-08 17:35:01 +0300

    proxy fix

--- mpd/__init__.py
+++ mpd/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from authlib.integrations.flask_client import OAuth
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 oauth = OAuth()
@@ -13,6 +14,7 @@ def create_app():
         DATABASE=os.path.join(app.instance_path, 'mpd.sqlite'),
         OAUTH_ID='',
         OAUTH_SECRET='',
+        PROXY=False,
     )
     app.config.from_pyfile('config.py', silent=True)
     os.makedirs(app.instance_path, exist_ok=True)
@@ -32,5 +34,9 @@ def create_app():
         client_kwargs={'scope': 'read_prefs'},
     )
     oauth.init_app(app)
+
+    if app.config['PROXY']:
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     return app
