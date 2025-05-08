cd85ceaec9b13bcec12dd203450ffed5530428d3
Ilya Zverev <ilya@zverev.info>
2025-05-08 17:54:14 +0300

    Fix area fill

--- mpd/party.py
+++ mpd/party.py
@@ -305,6 +305,9 @@ def piece_ed_plugin(party_id, piece_id):
         return redirect(url_for('party.list'))
 
     # Now prepare the zip file
+    piece['properties']['fill-opacity'] = 0
+    piece['properties']['stroke-width'] = 3
+    piece['properties']['stroke-color'] = '#3388ff'
     pluginyaml = f'''---
 id: mpd_{party_id}_{piece_id}
 name: Piece {piece_id} of {party['title']}
